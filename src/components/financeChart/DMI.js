af25657356abf868215a209ba5d317f6f3289c02
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2025-01-08 12:23:29 +0530

    DMI color changes

--- src/components/financeChart/DMI.js
+++ src/components/financeChart/DMI.js
@@ -10,36 +10,42 @@ import { timeFormat } from "d3-time-format";
 import CustomTooltip from "./CustomTooltip";
 
 const DMI = () => {
+  const adxColor = "red";
+  const plusDIColor = "blue";
+  const minusDIColor = "orange";
   return (
     <>
       <MouseCoordinateX rectWidth={60} displayFormat={timeFormat("%Y-%m-%d")} />
       <MouseCoordinateY rectWidth={60} displayFormat={format(".2f")} />
 
-      <CurrentCoordinate yAccessor={(d) => d.plusDI} fillStyle={"#000"} />
-      <CurrentCoordinate yAccessor={(d) => d.minusDI} fillStyle={"red"} />
-      <CurrentCoordinate yAccessor={(d) => d.adx} fillStyle={"blue"} />
+      <CurrentCoordinate yAccessor={(d) => d.plusDI} fillStyle={plusDIColor} />
+      <CurrentCoordinate
+        yAccessor={(d) => d.minusDI}
+        fillStyle={minusDIColor}
+      />
+      <CurrentCoordinate yAccessor={(d) => d.adx} fillStyle={adxColor} />
 
-      <LineSeries yAccessor={(d) => d.plusDI} strokeStyle="#000" />
-      <LineSeries yAccessor={(d) => d.minusDI} strokeStyle="red" />
-      <LineSeries yAccessor={(d) => d.adx} strokeStyle="blue" />
+      <LineSeries yAccessor={(d) => d.plusDI} strokeStyle={plusDIColor} />
+      <LineSeries yAccessor={(d) => d.minusDI} strokeStyle={minusDIColor} />
+      <LineSeries yAccessor={(d) => d.adx} strokeStyle={adxColor} />
 
       <CustomTooltip
         origin={[8, 32]}
-        yAccessor={(d) => d.plusDI}
-        tooltipName="DMI+"
-        textFill="#000"
+        yAccessor={(d) => d.adx}
+        tooltipName="ADX"
+        textFill={adxColor}
       />
       <CustomTooltip
         origin={[8, 48]}
-        yAccessor={(d) => d.minusDI}
-        tooltipName="DMI-"
-        textFill="red"
+        yAccessor={(d) => d.plusDI}
+        tooltipName="DI+"
+        textFill={plusDIColor}
       />
       <CustomTooltip
         origin={[8, 64]}
-        yAccessor={(d) => d.adx}
-        tooltipName="ADX"
-        textFill="blue"
+        yAccessor={(d) => d.minusDI}
+        tooltipName="DI-"
+        textFill={minusDIColor}
       />
     </>
   );
