94460b7b505671f447fa9ad3bcbec574960e86ea
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2025-07-10 23:51:39 +0530

    remove screener web scrapping for now

--- src/app/api/Fundamentals/route.js
+++ src/app/api/Fundamentals/route.js
@@ -1,90 +1,4 @@
 import { NextResponse } from "next/server";
-import axios from "axios";
-import * as cheerio from "cheerio";
-
-// This is for tempoary purpose need to remove this in future.
-async function getFinancialFundamentals(symbol) {
-  const url = `https://www.screener.in/company/${symbol}/consolidated/`;
-
-  try {
-    const response = await axios.get(url, {
-      headers: { "User-Agent": "Mozilla/5.0" },
-      timeout: 10000, // 10 second timeout
-    });
-
-    const $ = cheerio.load(response.data);
-    const epsValues = [];
-
-    $("tr.stripe").each((_, tr) => {
-      const firstCell = $(tr).find("td").first().text().trim();
-      if (firstCell.includes("EPS in Rs")) {
-        $(tr)
-          .find("td")
-          .slice(1)
-          .each((_, td) => {
-            const value = $(td).text().trim();
-            if (value && !isNaN(parseFloat(value))) {
-              epsValues.push(value);
-            }
-          });
-      }
-    });
-
-    let bookValue = null,
-      pe = "",
-      marketCap = "",
-      ROCE = "",
-      ROE = "",
-      price = "",
-      pb = "";
-
-    $("li.flex").each((_, li) => {
-      const label = $(li).find(".name").text().trim();
-      const numberval = $(li).find(".number").text().trim();
-
-      // Skip if no valid number value
-      if (!numberval) return;
-
-      if (label === "Book Value") {
-        bookValue = numberval;
-      } else if (label === "Stock P/E") {
-        pe = numberval;
-      } else if (label === "Market Cap") {
-        marketCap = numberval;
-      } else if (label === "ROE") {
-        ROE = numberval;
-      } else if (label === "ROCE") {
-        ROCE = numberval;
-      } else if (label === "Current Price") {
-        price = numberval;
-      }
-    });
-
-    if (price && bookValue) {
-      const priceNum = parseFloat(price.replace(/,/g, ""));
-      const bookValueNum = parseFloat(bookValue.replace(/,/g, ""));
-      if (!isNaN(priceNum) && !isNaN(bookValueNum) && bookValueNum !== 0) {
-        pb = (priceNum / bookValueNum).toFixed(2);
-      }
-    }
-
-    return {
-      symbol,
-      epsTTM: epsValues,
-      latestEPS: epsValues[epsValues.length - 1],
-      bookValue: bookValue,
-      pe,
-      pb,
-      marketCap,
-      ROE,
-      ROCE,
-      price,
-    };
-  } catch (error) {
-    console.error(`Error for ${symbol}:`, error.message);
-    return null;
-  }
-}
 
 export async function GET(req) {
   try {
@@ -106,7 +20,7 @@ export async function GET(req) {
       );
     }
 
-    const data = await getFinancialFundamentals(symbol);
+    const data = { data : 'coming soon'}
 
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
