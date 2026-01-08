d601416efa728b00d651a67683d96c75cdacb965
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2026-01-08 10:10:01 +0530

    seo update in main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,18 @@ import CandleStickChartClient from "./CandleStickChartClient";
 
 export const dynamic = "force-static";
 
+export const metadata = {
+  title: "Live NSE & BSE Stock Candlestick Charts | Riyaz Stocks",
+  description: "Free interactive candlestick charts for Indian stocks. Analyze NSE/BSE data with RSI, MACD, and technical indicators.",
+  keywords: ["NSE", "BSE", "Stock Charts", "Candlestick", "Technical Analysis"],
+};
+
 export default function Page() {
   return (
     <main>
+      <section style={{ padding: '5px', textAlign: 'center' }}>
+        <h1 style={{ fontSize: '20px' }}>Indian Stock Market Live Charts</h1>
+      </section>
       <CandleStickChartClient />
     </main>
   );
