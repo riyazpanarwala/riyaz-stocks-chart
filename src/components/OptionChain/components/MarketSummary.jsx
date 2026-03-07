5cc38f339050f3f4aee4d2f4ed6804527b2c89a2
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2026-03-07 17:27:45 +0530

    remove unnecessary heading

--- src/components/OptionChain/components/MarketSummary.jsx
+++ src/components/OptionChain/components/MarketSummary.jsx
@@ -3,16 +3,12 @@ const MarketSummary = ({ meta }) => {
     meta.PCR > 1 ? "Bullish" : meta.PCR < 0.9 ? "Bearish" : "Neutral";
 
   return (
-    <div>
-      <div className="summary-title">NIFTY {meta.spot}</div>
+    <div className="summary-grid">
+      <div>PCR : {meta.PCR?.toFixed(2)}</div>
+      <div>Sentiment : {sentiment}</div>
 
-      <div className="summary-grid">
-        <div>PCR : {meta.PCR?.toFixed(2)}</div>
-        <div>Sentiment : {sentiment}</div>
-
-        <div>Support : {meta.support?.join(", ")}</div>
-        <div>Resistance : {meta.resistance?.join(", ")}</div>
-      </div>
+      <div>Support : {meta.support?.join(", ")}</div>
+      <div>Resistance : {meta.resistance?.join(", ")}</div>
     </div>
   );
 };
