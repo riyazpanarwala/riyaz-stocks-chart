feb8bf29d0227135c1810521a2622642e36c378f
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2026-03-19 19:40:15 +0530

    New option chain page added

---
+++ src/app/optionchain/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import OptionChain from "../../components/OptionChainNew/index.jsx";
+
+export default function Page() {
+  return (
+    <div>
+      <OptionChain />
+    </div>
+  );
+}
