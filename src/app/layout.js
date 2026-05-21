cce97f072a9559e9fea84de5af158c5ff7e90225
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2026-05-21 20:02:25 +0530

    manifest.json added

--- src/app/layout.js
+++ src/app/layout.js
@@ -149,7 +149,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        {/* <link rel="manifest" href="/manifest.json" /> -- Need to add -- */}
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
         {/* JSON-LD structured data */}
